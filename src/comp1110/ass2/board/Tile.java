b74550b106f8b85e5b98ea7d98727cf06fc8d067
Haobo Zou <u7620014@anu.edu.au>
2023-08-20 23:29:07 +1000

    Added a getter method for Tile class.

--- src/comp1110/ass2/board/Tile.java
+++ src/comp1110/ass2/board/Tile.java
@@ -3,5 +3,9 @@ package comp1110.ass2.board;
 import comp1110.ass2.player.Rug;
 
 public class Tile {
-    private Rug visibleRug;
+    private Rug topRug;
+
+    public Rug getTopRug() {
+        return this.topRug;
+    }
 }
