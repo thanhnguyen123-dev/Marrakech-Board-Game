e29a995ee424fb85ac58da7bd4728918d20eb237
Haobo Zou <u7620014@anu.edu.au>
2023-08-20 18:37:26 +1000

    Added constructor and some fields for Rug class.

--- src/comp1110/ass2/player/Rug.java
+++ src/comp1110/ass2/player/Rug.java
@@ -1,4 +1,15 @@
 package comp1110.ass2.player;
 
+import comp1110.ass2.board.Coordinate;
+
 public class Rug {
+    private Player owner;
+    private int id;
+    private Coordinate[] rugTiles;
+
+    public Rug(Player owner, int id, Coordinate[] rugTiles) {
+        this.owner = owner;
+        this.id = id;
+        this.rugTiles = rugTiles;
+    }
 }
