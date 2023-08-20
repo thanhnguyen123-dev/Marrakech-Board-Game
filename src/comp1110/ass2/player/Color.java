fe05c0f2a03c6c8a0afb05d853539fd92d40aebb
Haobo Zou <u7620014@anu.edu.au>
2023-08-20 18:29:11 +1000

    renamed Color to Colour and fixed the values according to the rules

--- src/comp1110/ass2/player/Color.java
+++
@@ -1,5 +0,0 @@
-package comp1110.ass2.player;
-
-public enum Color {
-    RED, BLUE, GREEN, PURPLE
-}
---
+++ src/comp1110/ass2/player/Colour.java
@@ -0,0 +1,8 @@
+package comp1110.ass2.player;
+
+public enum Colour {
+    CYAN,
+    YELLOW,
+    RED,
+    PURPLE
+}
