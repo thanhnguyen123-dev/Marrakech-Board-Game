d597aae63df84b3ed4b8ddfa166fa8ca391053c6
Yaolin <819044267@qq.com>
2023-08-21 03:09:27 +1000

    Add Tilestate and position in Tile class

--- src/comp1110/ass2/board/Tile.java
+++ src/comp1110/ass2/board/Tile.java
@@ -6,9 +6,23 @@ import comp1110.ass2.player.Rug;
  * Tile class defines the state of a particular position on the board
  */
 public class Tile {
+    // Position of each tile on the board
+    private Coordinate position;
+    /**
+     * Used to judge whether a tile is empty or not
+     * If there is a rug, it's occupied
+     */
+    private enum TileState{
+        EMPTY, OCCUPIED;
+    }
+    // Store the state of tile
+    private TileState tileState;
     private Rug topRug;
 
     public Rug getTopRug() {
         return this.topRug;
+    }
+    public boolean isEmpty(){
+        return tileState==TileState.EMPTY;
     }
 }
