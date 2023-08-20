b413b120531866841bb6ef2d3d07780b0bf6c8ef
Haobo Zou <u7620014@anu.edu.au>
2023-08-20 18:34:47 +1000

    Modified Player class.

--- src/comp1110/ass2/player/Player.java
+++ src/comp1110/ass2/player/Player.java
@@ -1,12 +1,15 @@
 package comp1110.ass2.player;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Player {
-    private Player player1;
-    private Player player2;
-    private Player player3;
-    private Player player4;
+    private int dirhams = 30;
+    private int numOfRemainingRugs = 15;
+    private List placedRugs = new ArrayList<Rug>();
+    private Colour colour;
 
-    private static final int minPlayers = 2;
-
-    private static final int maxPlayers = 4;
+    public Player(Colour colour){
+        this.colour = colour;
+    }
 }
