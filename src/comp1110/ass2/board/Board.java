1c61f3399acd8705611e59a59dac8b35dc838ddc
Haobo Zou <u7620014@anu.edu.au>
2023-08-21 19:37:19 +1000

    Added some methods for Board class.

--- src/comp1110/ass2/board/Board.java
+++ src/comp1110/ass2/board/Board.java
@@ -6,11 +6,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    static final int NUM_OF_ROWS = 7;
-    static final int NUM_OF_COLS = 7;
+    private static final int NUM_OF_ROWS = 7;
+    private static final int NUM_OF_COLS = 7;
 
     private Tile[][] tiles = new Tile[Board.NUM_OF_ROWS][Board.NUM_OF_COLS];
     private List placedRugs = new ArrayList<Rug>();
     private Coordinate assamPosition = new Coordinate(3, 3);
     private Direction assamDirection = Direction.NORTH;
+
+    public Coordinate getAssamPosition() {
+        return this.assamPosition;
+    }
+
+    public void moveAssam(int steps) {
+        switch (assamDirection) {
+            case NORTH -> moveAssam(0, -steps);
+            case EAST -> moveAssam(steps, 0);
+            case SOUTH -> moveAssam(0, steps);
+            case WEST -> moveAssam(-steps, 0);
+        }
+    }
+
+    private void moveAssam(int x, int y) {
+        // FIXME
+    }
+
+    public void rotateAssam(int rotation) {
+        assamDirection.applyRotation(rotation);
+    }
+
+    public void placeRug(Rug rug) {
+        if (isRugValid(rug)) {
+            placedRugs.add(rug);
+            for (Coordinate position : rug.getRugTiles()) {
+                tiles[position.getX()][position.getY()].setTopRug(rug);
+            }
+        }
+    }
+
+    public boolean isRugValid(Rug rug) {
+        // FIXME
+        return false;
+    }
 }
