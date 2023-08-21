7b00600838120812cad315ae35b7cc7118bedf08
Haobo Zou <u7620014@anu.edu.au>
2023-08-22 08:20:40 +1000

    Added some more methods for GameState class.

--- src/comp1110/ass2/GameState.java
+++ src/comp1110/ass2/GameState.java
@@ -4,6 +4,7 @@ import comp1110.ass2.board.Board;
 import comp1110.ass2.player.Player;
 import comp1110.ass2.player.Rug;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,11 +17,48 @@ public class GameState {
     // All players
     private Player[] players;
     // Players are still in game
-    private List availablePlayers;
+    private List<Player> availablePlayers;
+    private Player currentPlayer;
 
     public GameState(Player[] players) {
         this.players = players;
-        this.availablePlayers = Arrays.asList(this.players);
+        this.availablePlayers = new ArrayList<Player>(Arrays.asList(this.players));
+        this.currentPlayer = this.availablePlayers.get(0);
+    }
+
+    public void nextPlayer() {
+        int index = this.availablePlayers.indexOf(this.currentPlayer);
+        this.currentPlayer = this.availablePlayers.get((index + 1) % this.availablePlayers.size());
+    }
+
+    public void removeCurrentPlayer() {
+        this.availablePlayers.remove(this.currentPlayer);
+    }
+
+    public boolean isGameOver() {
+        return this.currentPlayer.getNumOfUnplacedRugs() == 0;
+    }
+
+    public void rotateAssam(int rotation) {
+        this.board.rotateAssam(rotation);
+    }
+
+    public void moveAssam(int steps) {
+        this.board.moveAssam(steps);
+    }
+
+    public boolean isPaymentRequired() {
+        // FIXME
+        return true;
+    }
+
+    public int calculatePayment() {
+        // FIXME
+        return 0;
+    }
+
+    public void makePayment(int amount) {
+        this.currentPlayer.makePayment(this.board.getAssamRug().getOwner(), amount);
     }
 
     public boolean isPlacementValid(Rug rug) {
@@ -28,7 +66,7 @@ public class GameState {
     }
 
     public void makePlacement(Rug rug) {
-        rug.getOwner().placeRug();
+        this.currentPlayer.placeRug();
         board.placeRug(rug);
     }
 }
