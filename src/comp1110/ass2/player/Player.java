5bc920acb2b27bbff1c289b56e0df78dfe6d0a61
Haobo Zou <u7620014@anu.edu.au>
2023-09-17 20:08:16 +1000

    Added Javadoc for the Player class.

--- src/comp1110/ass2/player/Player.java
+++ src/comp1110/ass2/player/Player.java
@@ -6,11 +6,11 @@ package comp1110.ass2.player;
 public class Player {
     private int dirham;
     private int numOfUnplacedRugs;
-    private Colour colour;
+    private final Colour colour;
 
     /**
      * Constructor: creates an instance of the Player class
-     * @param colour
+     * @param colour the Colour of the Player
      */
     public Player(Colour colour) {
         this.dirham = 30;
@@ -19,33 +19,37 @@ public class Player {
     }
 
     /**
-     * getter method for the current dirhams value of a player
-     * @return dirham
+     * @return the amount of dirhams the Player has
      */
     public int getDirham() {
         return dirham;
     }
 
     /**
-     * getter method for the number of unplaced rugs of a player
-     * @return numOfUnplacedRugs
+     * @return the number of unplaced Rugs the Player has
      */
     public int getNumOfUnplacedRugs() {
         return numOfUnplacedRugs;
     }
 
+    /**
+     * @return the Colour of the Player
+     */
     public Colour getColour() {
         return this.colour;
     }
 
+    /**
+     * Places a Rug on the Board, number of unplaced Rugs decreases by 1
+     */
     public void placeRug() {
         this.numOfUnplacedRugs--;
     }
 
     /**
-     * If Assam is on other player's rug after movement, player need to pay dirhams
-     * @param otherPlayer
-     * @param amount
+     * Pays a certain amount of dirhams to the other Player
+     * @param otherPlayer the other Player
+     * @param amount amount of dirhams to be paid
      */
     public void makePayment(Player otherPlayer, int amount) {
         this.dirham -= amount;
@@ -55,7 +59,7 @@ public class Player {
     /**
      * Constructor: creates an instance of the Player class
      * Decode the playerString to get corresponding value for instance fields
-     * @param playerString
+     * @param playerString string representation for the Player
      */
     public Player(String playerString) {
         this.dirham = Integer.parseInt(playerString.substring(1, 4), 10);
