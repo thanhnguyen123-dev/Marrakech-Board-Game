7521a148a381d58453325585b4713c5e48f5e91e
Haobo Zou <u7620014@anu.edu.au>
2023-09-26 18:53:21 +1000

    Implemented nextPlayer(), removeCurrentPlayer(), isPlayerAvailable() for GameStateTest.

--- tests/comp1110/ass2/GameStateTest.java
+++ tests/comp1110/ass2/GameStateTest.java
@@ -1,21 +1,48 @@
 package comp1110.ass2;
 
+import comp1110.ass2.player.Colour;
+import comp1110.ass2.player.Player;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameStateTest {
+    Player player1 = new Player(Colour.YELLOW);
+    Player player2 = new Player(Colour.RED);
+    Player player3 = new Player(Colour.CYAN);
+    Player player4 = new Player(Colour.PURPLE);
+    Player[] players = new Player[]{this.player1, this.player2, this.player3, this.player4};
 
     @Test
     void nextPlayer() {
+        GameState gameState = new GameState(this.players);
+        Assertions.assertEquals(this.player1, gameState.getCurrentPlayer());
+        gameState.nextPlayer();
+        Assertions.assertEquals(this.player2, gameState.getCurrentPlayer());
+        gameState.nextPlayer();
+        Assertions.assertEquals(this.player3, gameState.getCurrentPlayer());
+        gameState.nextPlayer();
+        Assertions.assertEquals(this.player4, gameState.getCurrentPlayer());
+        gameState.nextPlayer();
+        Assertions.assertEquals(this.player1, gameState.getCurrentPlayer());
     }
 
     @Test
     void removeCurrentPlayer() {
+        GameState gameState = new GameState(this.players);
+        Assertions.assertEquals(this.player1, gameState.getCurrentPlayer());
+        gameState.removeCurrentPlayer();
+        Assertions.assertEquals(this.player1, gameState.getCurrentPlayer());
+        Assertions.assertFalse(gameState.isPlayerAvailable(gameState.getCurrentPlayer()));
     }
 
     @Test
     void isPlayerAvailable() {
+        GameState gameState = new GameState(this.players);
+        for (Player player : gameState.getPlayers()) {
+            Assertions.assertTrue(gameState.isPlayerAvailable(player));
+        }
     }
 
     @Test
