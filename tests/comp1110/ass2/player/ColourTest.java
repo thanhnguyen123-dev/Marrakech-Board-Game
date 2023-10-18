69044dedd6ee34e6d2fb209c804195f63010f11d
Yaolin <819044267@qq.com>
2023-10-18 18:43:29 +1100

    Finish ColourTest, TileTest

--- tests/comp1110/ass2/player/ColourTest.java
+++ tests/comp1110/ass2/player/ColourTest.java
@@ -9,14 +9,18 @@ class ColourTest {
 
     @Test
     void charToColour() {
+        assertEquals(Colour.YELLOW, Colour.charToColour('y'));
+        assertEquals(Colour.RED, Colour.charToColour('r'));
+        assertEquals(Colour.CYAN, Colour.charToColour('c'));
+        assertEquals(Colour.PURPLE, Colour.charToColour('p'));
     }
 
     @Test
     void isColourValid() {
-        Assertions.assertTrue(Colour.isColourValid('y'));
-        Assertions.assertTrue(Colour.isColourValid('r'));
-        Assertions.assertTrue(Colour.isColourValid('c'));
-        Assertions.assertTrue(Colour.isColourValid('p'));
+        assertTrue(Colour.isColourValid('y'));
+        assertTrue(Colour.isColourValid('r'));
+        assertTrue(Colour.isColourValid('c'));
+        assertTrue(Colour.isColourValid('p'));
         for (int i = 0; i < 26; i++) {
             char colour = (char) (i + 97);
             if (colour != 'y' && colour != 'r' && colour != 'c' && colour != 'p') {
@@ -26,10 +30,10 @@ class ColourTest {
     }
 
     @Test
-    void colourToString() {
-    }
-
-    @Test
-    void getFrontEndColor() {
+    void testToString() {
+        assertEquals("YELLOW", Colour.YELLOW.toString());
+        assertEquals("RED", Colour.RED.toString());
+        assertEquals("CYAN", Colour.CYAN.toString());
+        assertEquals("PURPLE", Colour.PURPLE.toString());
     }
 }
