27fc018126c5fe595349914616026210d72cffde
Haobo Zou <zouhaobo@hotmail.com>
2023-10-18 18:39:30 +1100

    Updated player stats display.

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -19,6 +19,8 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -39,7 +41,7 @@ public class Game extends Application {
     private static final double CONTROL_AREA_WIDTH = PLAYER_AREA_WIDTH;
     private static final double CONTROL_AREA_HEIGHT = PLAYER_AREA_HEIGHT - STATS_AREA_HEIGHT - MARGIN_TOP;
     static final Color TILE_COLOR = Color.TAN;
-    private static final Color GAME_PANE_BORDER_COLOR = TILE_COLOR.darker();
+    private static final Color GAME_PANE_BORDER_COLOR = TILE_COLOR.darker().darker();
     private static final BorderStrokeStyle GAME_PANE_BORDER_STROKE_STYLE = BorderStrokeStyle.SOLID;
     private static final CornerRadii GAME_PANE_BORDER_RADII = new CornerRadii(24);
     private static final BorderWidths GAME_PANE_BORDER_WIDTH = new BorderWidths(4);
@@ -57,11 +59,17 @@ public class Game extends Application {
     static final CornerRadii COLOUR_BUTTON_BORDER_RADII = GAME_PANE_BORDER_RADII;
     static final BorderWidths COLOUR_BUTTON_BORDER_WIDTH = new BorderWidths(8);
     static final double HIGHLIGHTED_OPACITY = 1 - (Math.sqrt(5) - 1) / 2;
+    private static final BorderWidths ASSAM_BORDER_WIDTH = new BorderWidths(4);
     private static final double ASSAM_SIDE = (Math.sqrt(5) - 1) / 2 * TILE_SIDE;
     private static final double ASSAM_RELOCATION = (TILE_SIDE - ASSAM_SIDE) / 2;
     private static final Color ASSAM_COLOR = Color.SPRINGGREEN.darker();
     //https://fonts.google.com/icons?selected=Material%20Symbols%20Rounded%3Anavigation%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024
     private static final String ASSAM_SVG = "M480-240 222-130q-13 5-24.5 2.5T178-138q-8-8-10.5-20t2.5-25l273-615q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l273 615q5 13 2.5 25T782-138q-8 8-19.5 10.5T738-130L480-240Z";
+
+    private static final Font BUTTON_FONT = Font.font("Verdana", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 18);
+    private static final Font GENERAL_TEXT_FONT_REGULAR = Font.font("Verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 14);
+    private static final Font GENERAL_TEXT_FONT_ITALIC = Font.font("Verdana", FontWeight.SEMI_BOLD, FontPosture.ITALIC, 14);
+    private static final Font PLAYER_COLOUR_TEXT_FONT_REGULAR_18 = Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 24);
 
     private final Pane allTiles = new Pane();
     private final Pane placedRugs = new Pane();
@@ -87,25 +95,25 @@ public class Game extends Application {
     private final ArrayList<Player> tmp = new ArrayList<>();
     private ArrayList<PlayerSelector> playerSelectors;
 
-    VBox eachPlayerStatArea = new VBox();
+    private final VBox playerStatsVBox = new VBox();
 
     private final GameButton btnNumberConfirm = new GameButton("Confirm", BUTTON_WIDTH, BUTTON_HEIGHT);
     private final GameButton btnColourConfirm = new GameButton("Confirm", BUTTON_WIDTH, BUTTON_HEIGHT);
 
     private final GameButton btnInitialRotation = new GameButton("Rotate Right", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
-    private final GameButton btnConfirmInitialRotation = new GameButton("Confirm Rotation", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
+    private final GameButton btnConfirmInitialRotation = new GameButton("Confirm", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
     private ArrayList<GameButton> btnRotations;
-    private final GameButton btnConfirmRotation = new GameButton("Confirm Rotation", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
+    private final GameButton btnConfirmRotation = new GameButton("Confirm", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
     private final GameButton btnRollDie = new GameButton("Roll Die", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
-    private final GameButton btnMoveAssam = new GameButton("Confirm Movement", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
+    private final GameButton btnMoveAssam = new GameButton("Proceed", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
     private final GameButton btnConfirmPayment = new GameButton("Proceed", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
-    private final GameButton btnRotateRug = new GameButton("Rotate Rug", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
-    private final GameButton btnConfirmPlacement = new GameButton("Confirm Placement", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
+    private final GameButton btnRotateRug = new GameButton("Rotate", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
+    private final GameButton btnConfirmPlacement = new GameButton("Confirm", BUTTON_WIDTH * 1.2, BUTTON_HEIGHT);
 
     private final Text movementText = new Text();
     private final Text paymentText = new Text();
 
-    private final long MILLIS = 1500;
+    private final long MILLIS = 10;
 
     // all players, including human and computer players
     private Player[] players;
@@ -117,21 +125,22 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Home page, title screen
+        // Home page, titleText screen
         StackPane titleRootPane = new StackPane();
-        VBox titlePane = new VBox();
+        VBox titleVBox = new VBox();
         BackgroundImage background = drawBackground();
         titleRootPane.setBackground(new Background(background));
 
-        Text title = new Text("Play Marrakech!");
-        title.setFont(new Font("", 60));
+        Text titleText = new Text("Marrakech");
+        titleText.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 60));
         GameButton btnStart = new GameButton("Start", BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnStart.setFont(BUTTON_FONT);
         // Make two elements center vertical
-        titlePane.setAlignment(Pos.CENTER);
-        titlePane.setSpacing(20);
-        // Add all children of titlePane
-        titlePane.getChildren().addAll(title, btnStart);
-        titleRootPane.getChildren().add(titlePane);
+        titleVBox.setAlignment(Pos.CENTER);
+        titleVBox.setSpacing(20);
+        // Add all children of titleVBox
+        titleVBox.getChildren().addAll(titleText, btnStart);
+        titleRootPane.getChildren().add(titleVBox);
         Scene titleScene = new Scene(titleRootPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 
         // Choose number of players
@@ -140,11 +149,11 @@ public class Game extends Application {
         numberRootPane.setBackground(new Background(background));
         Scene numberScene = new Scene(numberRootPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        // Choice box to choose the number of human players
         Text numberText = new Text("Please choose the number of players:");
-        numberText.setFont(new Font(18));
+        numberText.setFont(Font.font("Verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 20));
         numberText.relocate(WINDOW_WIDTH / 2.0 - BUTTON_WIDTH / 2.0 - 300, 280);
 
+        // Choice box to choose the number of human players
         ChoiceBox<Integer> choiceBox = new ChoiceBox<>();
         choiceBox.setMinWidth(BUTTON_WIDTH);
         choiceBox.setMaxWidth(BUTTON_WIDTH);
@@ -155,7 +164,9 @@ public class Game extends Application {
 
         //Back and Confirm buttons
         GameButton btnNumberBack = new GameButton("Back", BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnNumberBack.setFont(BUTTON_FONT);
         btnNumberBack.relocate(BUTTON_HEIGHT / 2.0, BUTTON_HEIGHT / 2.0);
+        this.btnNumberConfirm.setFont(BUTTON_FONT);
         this.btnNumberConfirm.relocate(WINDOW_WIDTH / 2.0 - BUTTON_WIDTH / 2.0, 360);
         this.btnNumberConfirm.requestFocus();
 
@@ -173,14 +184,17 @@ public class Game extends Application {
 
         //Back, Reset and Confirm buttons
         GameButton btnColourBack = new GameButton("Back", BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnColourBack.setFont(BUTTON_FONT);
         btnColourBack.relocate(BUTTON_HEIGHT / 2.0, BUTTON_HEIGHT / 2.0);
         GameButton btnColourReset = new GameButton("Reset", BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnColourReset.setFont(BUTTON_FONT);
         btnColourReset.relocate(WINDOW_WIDTH / 2.0 - BUTTON_WIDTH * 1.5, 420);
+        this.btnColourConfirm.setFont(BUTTON_FONT);
         this.btnColourConfirm.relocate(WINDOW_WIDTH / 2.0 + BUTTON_WIDTH * 0.5, 420);
         this.btnColourConfirm.setDisable(true);
 
         colourPane.getChildren().addAll(this.playerSelectors);
-        colourPane.getChildren().addAll(btnColourBack, btnColourReset, btnColourConfirm);
+        colourPane.getChildren().addAll(btnColourBack, btnColourReset, this.btnColourConfirm);
         colourRootPane.getChildren().add(colourPane);
 
         // Start button on the homepage
@@ -285,8 +299,8 @@ public class Game extends Application {
         playerArea.getChildren().add(statsArea);
         // Display game stats
         this.phaseText = new Text("Current Game Phase: " + this.currentPhase.toString());
-        this.phaseText.setFont(new Font(20));
-        this.phaseText.relocate(30, 10);
+        this.phaseText.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.ITALIC, 18));
+        this.phaseText.relocate(20, 10);
         statsArea.getChildren().add(this.phaseText);
 
         // Display player stats
@@ -295,7 +309,7 @@ public class Game extends Application {
         playerStatArea.relocate(30, 50);
         // load player stats
         updatePlayerStats();
-        playerStatArea.getChildren().addAll(eachPlayerStatArea);
+        playerStatArea.getChildren().addAll(playerStatsVBox);
         statsArea.getChildren().add(playerStatArea);
 
         // control area
@@ -305,29 +319,39 @@ public class Game extends Application {
         playerArea.getChildren().add(this.controlArea);
 
         //buttons inside control area
+        this.btnInitialRotation.setFont(BUTTON_FONT);
         this.btnInitialRotation.relocate(BUTTON_WIDTH, BUTTON_HEIGHT / 2.0);
+        this.btnConfirmInitialRotation.setFont(BUTTON_FONT);
         this.btnConfirmInitialRotation.relocate(BUTTON_WIDTH, BUTTON_HEIGHT * 2);
 
-        GameButton btnRotateLeft = new GameButton("Rotate Left", BUTTON_WIDTH * 0.8, BUTTON_HEIGHT);
-        btnRotateLeft.relocate(BUTTON_WIDTH * 0.6, BUTTON_HEIGHT / 2.0);
-        GameButton btnRotateRight = new GameButton("Rotate Right", BUTTON_WIDTH * 0.8, BUTTON_HEIGHT);
-        btnRotateRight.relocate(BUTTON_WIDTH * 1.8, BUTTON_HEIGHT / 2.0);
+        GameButton btnRotateLeft = new GameButton("Rotate Left", BUTTON_WIDTH * 1.4, BUTTON_HEIGHT);
+        btnRotateLeft.setFont(BUTTON_FONT);
+        btnRotateLeft.relocate(BUTTON_WIDTH * 0.1, BUTTON_HEIGHT / 2.0);
+        GameButton btnRotateRight = new GameButton("Rotate Right", BUTTON_WIDTH * 1.4, BUTTON_HEIGHT);
+        btnRotateRight.setFont(BUTTON_FONT);
+        btnRotateRight.relocate(BUTTON_WIDTH * 1.67, BUTTON_HEIGHT / 2.0);
         this.btnRotations = new ArrayList<>(List.of(btnRotateLeft, btnRotateRight));
+        this.btnConfirmRotation.setFont(BUTTON_FONT);
         this.btnConfirmRotation.relocate(BUTTON_WIDTH, BUTTON_HEIGHT * 2);
 
+        this.btnRollDie.setFont(BUTTON_FONT);
         this.btnRollDie.relocate(BUTTON_WIDTH, BUTTON_HEIGHT * 2);
 
         this.movementText.relocate(BUTTON_WIDTH * 0.4, BUTTON_HEIGHT);
-        this.movementText.setFont(new Font(16));
+        this.movementText.setFont(GENERAL_TEXT_FONT_REGULAR);
         this.movementText.setWrappingWidth(300);
+        this.btnMoveAssam.setFont(BUTTON_FONT);
         this.btnMoveAssam.relocate(BUTTON_WIDTH, BUTTON_HEIGHT * 2);
 
         this.paymentText.relocate(BUTTON_WIDTH * 0.4, BUTTON_HEIGHT);
-        this.paymentText.setFont(new Font(16));
+        this.paymentText.setFont(GENERAL_TEXT_FONT_REGULAR);
         this.paymentText.setWrappingWidth(300);
+        this.btnConfirmPayment.setFont(BUTTON_FONT);
         this.btnConfirmPayment.relocate(BUTTON_WIDTH, BUTTON_HEIGHT * 2);
 
+        this.btnRotateRug.setFont(BUTTON_FONT);
         this.btnRotateRug.relocate(BUTTON_WIDTH * 1.5, BUTTON_HEIGHT / 2.0);
+        this.btnConfirmPlacement.setFont(BUTTON_FONT);
         this.btnConfirmPlacement.relocate(BUTTON_WIDTH * 1.5, BUTTON_HEIGHT * 2);
 
         //allows player or computer to set Assam's initial direction
@@ -415,6 +439,9 @@ public class Game extends Application {
                     updatePlayerStats();
                     if (!this.gameState.isGameOver()) {
                         nextTurn();
+                    } else {
+                        updatePlayerStats();
+                        this.phaseText.setText("Game Over");
                     }
                 }
             }
@@ -477,6 +504,9 @@ public class Game extends Application {
 
             if (!this.gameState.isGameOver()) {
                 nextTurn();
+            } else {
+                updatePlayerStats();
+                this.phaseText.setText("Game Over");
             }
         }
     }
@@ -573,6 +603,9 @@ public class Game extends Application {
                 updatePlayerStats();
                 if (!this.gameState.isGameOver()) {
                     nextTurn();
+                } else {
+                    updatePlayerStats();
+                    this.phaseText.setText("Game Over");
                 }
             }
         });
@@ -596,6 +629,9 @@ public class Game extends Application {
 
             if (!this.gameState.isGameOver()) {
                 nextTurn();
+            } else {
+                updatePlayerStats();
+                this.phaseText.setText("Game Over");
             }
         });
     }
@@ -690,7 +726,7 @@ public class Game extends Application {
         this.assam.setMinSize(ASSAM_SIDE, ASSAM_SIDE);
         this.assam.setMaxSize(ASSAM_SIDE, ASSAM_SIDE);
         this.assam.setBackground(new Background(new BackgroundFill(ASSAM_COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
-        this.assam.setBorder(new Border(new BorderStroke(ASSAM_COLOR.darker(), GAME_PANE_BORDER_STROKE_STYLE, GAME_PANE_BORDER_RADII, GAME_PANE_BORDER_WIDTH)));
+        this.assam.setBorder(new Border(new BorderStroke(ASSAM_COLOR.darker(), GAME_PANE_BORDER_STROKE_STYLE, GAME_PANE_BORDER_RADII, ASSAM_BORDER_WIDTH)));
         updateAssam();
     }
 
@@ -710,23 +746,33 @@ public class Game extends Application {
      * Update player stats
      */
     private void updatePlayerStats() {
-        eachPlayerStatArea.getChildren().clear();
-        eachPlayerStatArea.setSpacing(5);
+        this.playerStatsVBox.getChildren().clear();
+        this.playerStatsVBox.setSpacing(4);
         for (Player player : this.gameState.getPlayers()) {
-            if (player.isComputer()) {
-                Text colourText = new Text("Player " + player.getColour() + " (Computer: " + player.getStrategy() + ")");
-                colourText.setFont(new Font(16));
-                eachPlayerStatArea.getChildren().add(colourText);
+            String colourString = "PLAYER " + player.getColour();
+            Text colourText = new Text();
+            colourText.setFont(PLAYER_COLOUR_TEXT_FONT_REGULAR_18);
+            if (this.gameState.isPlayerAvailable(player)) {
+                colourText.setFill(Colour.getFrontEndColor(player.getColour()));
             } else {
-                Text colourText = new Text("Player " + player.getColour());
-                colourText.setFont(new Font(16));
-                eachPlayerStatArea.getChildren().add(colourText);
+                colourText.setFill(Color.GRAY);
+                colourString += " - OUT";
             }
-            Text dirhamText = new Text("Dirham: " + player.getDirham());
-            dirhamText.setFont(new Font(16));
-            Text numOfUnplacedRugsText = new Text("Number of unplaced rugs: " + player.getNumOfUnplacedRugs());
-            numOfUnplacedRugsText.setFont(new Font(16));
-            eachPlayerStatArea.getChildren().addAll(dirhamText, numOfUnplacedRugsText);
+            colourText.setText(colourString);
+
+            String scoreString = "     Current Score: " + this.gameState.getPlayerScore(player);
+            Text scoreText = new Text();
+            scoreText.setFont(GENERAL_TEXT_FONT_ITALIC);
+            if (player.isComputer()) {
+                scoreText.setText("COMPUTER - " + player.getStrategy() + scoreString);
+            } else {
+                scoreText.setText("HUMAN" + scoreString);
+            }
+
+            Text statsText = new Text(player.getDirham() + " dirhams     " + player.getNumOfUnplacedRugs() + " rugs remaining");
+            statsText.setFont(GENERAL_TEXT_FONT_ITALIC);
+
+            playerStatsVBox.getChildren().addAll(colourText, scoreText, statsText);
         }
     }
 
@@ -758,16 +804,20 @@ public class Game extends Application {
 
             this.chbIsComputer = new CheckBox();
             this.chbIsComputer.setText("Computer");
+            this.chbIsComputer.setFont(GENERAL_TEXT_FONT_ITALIC);
             this.chbIsComputer.relocate(0, COLOUR_BUTTON_RADIUS * 2.2);
 
             this.lblStrategy = new Label("Strategy:");
+            this.lblStrategy.setFont(GENERAL_TEXT_FONT_REGULAR);
             this.lblStrategy.relocate(0, COLOUR_BUTTON_RADIUS * 2.6);
 
             ToggleGroup toggleGroup = new ToggleGroup();
             this.rbRandom = new RadioButton("Random");
+            this.rbRandom.setFont(GENERAL_TEXT_FONT_ITALIC);
             this.rbRandom.relocate(0, COLOUR_BUTTON_RADIUS * 3.0);
             this.rbRandom.setToggleGroup(toggleGroup);
             this.rbIntelligent = new RadioButton("Intelligent");
+            this.rbIntelligent.setFont(GENERAL_TEXT_FONT_ITALIC);
             this.rbIntelligent.relocate(0, COLOUR_BUTTON_RADIUS * 3.4);
             this.rbIntelligent.setToggleGroup(toggleGroup);
             this.rbIntelligent.setDisable(true);
