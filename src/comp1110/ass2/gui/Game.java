bd12961c272c1eb8d6505646e4a8384cfe3dd9ef
Yaolin <819044267@qq.com>
2023-10-19 18:47:58 +1100

    modify hint button and information

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,9 +1,9 @@
 package comp1110.ass2.gui;
 
 import comp1110.ass2.GameState;
-import comp1110.ass2.board.Die;
 import comp1110.ass2.board.Tile;
 import comp1110.ass2.player.Colour;
+import comp1110.ass2.board.Die;
 import comp1110.ass2.player.Player;
 import comp1110.ass2.player.Player.Strategy;
 import comp1110.ass2.player.Rug;
@@ -25,6 +25,7 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.application.Platform;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,6 +66,7 @@ public class Game extends Application {
     private static final double ASSAM_SIDE = (Math.sqrt(5) - 1) / 2 * TILE_SIDE;
     private static final double ASSAM_RELOCATION = (TILE_SIDE - ASSAM_SIDE) / 2;
     private static final Color ASSAM_COLOR = Color.SPRINGGREEN.darker();
+    //https://fonts.google.com/icons?selected=Material%20Symbols%20Rounded%3Anavigation%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024
     private static final String ASSAM_SVG = "M480-240 222-130q-13 5-24.5 2.5T178-138q-8-8-10.5-20t2.5-25l273-615q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l273 615q5 13 2.5 25T782-138q-8 8-19.5 10.5T738-130L480-240Z";
     private static final Font GENERAL_TEXT_FONT_REGULAR = Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 14);
     private static final Font GENERAL_TEXT_FONT_ITALIC = Font.font("Verdana", FontWeight.BOLD, FontPosture.ITALIC, 14);
@@ -389,18 +391,50 @@ public class Game extends Application {
 
         //allows player or computer to set Assam's initial direction
         if (this.gameState.getCurrentPlayer().isComputer()) {
+            // For computer player's turn, disable button
             this.btnMainBack.setDisable(true);
+            this.hintButton.setDisable(true);
             this.btnInitialRotation.setDisable(true);
             this.btnConfirmInitialRotation.setDisable(true);
             simulateInitialRotation();
         }
         this.controlArea.getChildren().addAll(this.btnInitialRotation, this.btnConfirmInitialRotation);
 
+        // Click hint button to display game rules
         hintButton.setOnMouseClicked(event -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Hint");
-            alert.setHeaderText(null);
-            alert.setContentText("Try to have more dirhams!");
+            // Modify the default icon
+            ImageView newIcon = new ImageView(new Image("resources/hint.png"));
+            newIcon.setFitWidth(40);
+            newIcon.setFitHeight(40);
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            stage.getIcons().add(new Image("resources/hint.png"));
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.setGraphic(newIcon);
+            // Set background colour of dialog
+            dialogPane.setStyle("-fx-background-color: #eecdab; -fx-padding: 15px;");
+            dialogPane.setPrefSize(400, 250);
+            // Different hints in different phases
+            alert.setHeaderText("In " + currentPhase + " phase");
+            if (currentPhase == Phase.ROTATION) {
+                alert.setContentText("You can use 'Rotate Left' and 'Rotate Right' to rotate direction of Assam or current rug, " +
+                        "then click 'Confirm.");
+            } else if (currentPhase == Phase.MOVEMENT) {
+                alert.setContentText("You can 'Roll Die' to make Assam move, " +
+                        "if Assam lands on the rug of another player who is still in the game," +
+                        " you need to pay that player a certain amount of dirhams.");
+            } else if (currentPhase == Phase.PLACEMENT) {
+                alert.setContentText("You can rotate and drag the rug onto the board, " +
+                        "trying to make your rug occupy as much space as possible.");
+            }
+            // Set style of Alert dialogPane
+            Platform.runLater(() -> {
+                dialogPane.lookup(".label").setStyle("-fx-font-size: 18px; -fx-font-weight: bold;" + "-fx-text-fill:" + TEXT_FILL);
+                dialogPane.lookup(".button").setStyle("-fx-text-fill: white;-fx-background-color: #672F09;");
+                dialogPane.lookup(".header-panel").setStyle("-fx-background-color: #eecdab");
+                dialogPane.lookup(".content").setStyle("-fx-font-size: 16px;" + "-fx-text-fill:" + TEXT_FILL);
+            });
             alert.showAndWait();
         });
         this.btnInitialRotation.setOnMouseClicked(event -> this.assam.setRotate(this.assam.getRotate() + 90));
@@ -490,6 +524,7 @@ public class Game extends Application {
                         nextTurn();
                     } else {
                         this.btnMainBack.setDisable(false);
+                        this.hintButton.setDisable(false);
                     }
                 }
             }
@@ -565,6 +600,7 @@ public class Game extends Application {
                 nextTurn();
             } else {
                 this.btnMainBack.setDisable(false);
+                this.hintButton.setDisable(false);
             }
         }
     }
@@ -695,6 +731,7 @@ public class Game extends Application {
                     nextTurn();
                 } else {
                     this.btnMainBack.setDisable(false);
+                    this.hintButton.setDisable(false);
                 }
             }
         });
@@ -730,6 +767,7 @@ public class Game extends Application {
                 nextTurn();
             } else {
                 this.btnMainBack.setDisable(false);
+                this.hintButton.setDisable(false);
             }
         });
     }
@@ -791,12 +829,15 @@ public class Game extends Application {
         this.controlArea.getChildren().add(this.btnConfirmRotation);
 
         if (this.gameState.getCurrentPlayer().isComputer()) {
+            // For computer player's turn, disable button
             this.btnMainBack.setDisable(true);
+            this.hintButton.setDisable(true);
             this.btnRotations.forEach(b -> b.setDisable(true));
             this.btnConfirmRotation.setDisable(true);
             simulateRotation();
         } else {
             this.btnMainBack.setDisable(false);
+            this.hintButton.setDisable(false);
         }
     }
 
