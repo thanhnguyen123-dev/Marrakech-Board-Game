e0da0c6dbd43f1f82844091bdd7156d950bbb2d4
Haobo Zou <zouhaobo@hotmail.com>
2023-10-19 18:37:30 +1100

    Added Javadoc for GameInvisibleRug class.

--- src/comp1110/ass2/gui/GameInvisibleRug.java
+++ src/comp1110/ass2/gui/GameInvisibleRug.java
@@ -4,10 +4,22 @@ import comp1110.ass2.gui.Game.Orientation;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * a custom rectangle class used to represent all the possible rug placements on the board inside Game class
+ * @author u7620014 Haobo Zou
+ */
 class GameInvisibleRug extends Rectangle {
     private final GameTile[] gameTiles = new GameTile[2];
     private final Orientation orientation;
 
+    /**
+     * Constructor: creates an instance of the GameInvisibleRug class
+     * @param x the x-axis relocation of its centre
+     * @param y the y-axis relocation of its centre
+     * @param gameTiles an array consisting of the two game tiles occupied by the invisible rug
+     * @param orientation the orientation of the invisible rug
+     * @author u7620014 Haobo Zou
+     */
     public GameInvisibleRug(double x, double y, GameTile[] gameTiles, Orientation orientation) {
         super(0, 0, Game.TILE_SIDE, 2 * Game.TILE_SIDE);
         this.gameTiles[0] = gameTiles[0];
@@ -24,14 +36,31 @@ class GameInvisibleRug extends Rectangle {
         this.setOpacity(0);
     }
 
+    /**
+     * getter method for orientation
+     * @return the orientation of the invisible rug
+     * @author u7620014 Haobo Zou
+     */
     public Orientation getOrientation() {
         return this.orientation;
     }
 
+    /**
+     * getter method for gameTiles
+     * @return an array consisting of the two game tiles occupied by the invisible rug
+     * @author u7620014 Haobo Zou
+     */
     public GameTile[] getGameTiles() {
         return this.gameTiles;
     }
 
+    /**
+     * Calculates the distance between the centre of the invisible rug and a given position
+     * @param x the x coordinate of the given position
+     * @param y the y coordinate of the given position
+     * @return the distance between the centre of the invisible rug and the position
+     * @author u7620014 Haobo Zou
+     */
     public double distance(double x, double y) {
         return Math.sqrt((this.getLayoutX() - x) * (this.getLayoutX() - x) + (this.getLayoutY() - y) * (this.getLayoutY() - y));
     }
