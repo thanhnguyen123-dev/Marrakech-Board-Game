984ba17dd696697f91d434f184547fab50d49e48
Haobo Zou <zouhaobo@hotmail.com>
2023-10-19 15:08:05 +1100

    Added more Javadoc for the Player class.

--- src/comp1110/ass2/player/Player.java
+++ src/comp1110/ass2/player/Player.java
@@ -1,7 +1,8 @@
 package comp1110.ass2.player;
 
 /**
- * Player class contains properties of each Player
+ * Player class contains properties of each player
+ * @author u7594144 Le Thanh Nguyen, u7582846 Yaolin Li, u7620014 Haobo Zou
  */
 public class Player {
     private int dirham;
@@ -26,7 +27,7 @@ public class Player {
 
     /**
      * getter method for dirham
-     * @return the amount of dirhams that the player has
+     * @return the amount of dirhams possessed by the player
      * @author Le Thanh Nguyen u7594144
      */
     public int getDirham() {
@@ -35,7 +36,7 @@ public class Player {
 
     /**
      * getter method for numOfUnplacedRugs
-     * @return the number of unplaced rugs that the player has
+     * @return the number of unplaced rugs possessed by the player
      * @author Le Thanh Nguyen u7594144
      */
     public int getNumOfUnplacedRugs() {
@@ -53,23 +54,36 @@ public class Player {
 
     /**
      * getter method for isComputer
-     * @return True if is a computer player, false if is a human player
+     * @return true if the player is a computer player, false if the player is a human player
+     * @author u7620014 Haobo Zou
      */
     public boolean isComputer() {
         return this.isComputer;
     }
 
     /**
-     * set isComputer attribute of player
+     * setter method for isComputer
+     * @param isComputer if the player is a computer player or not
+     * @author u7620014 Haobo Zou
      */
     public void setIsComputer(boolean isComputer) {
         this.isComputer = isComputer;
     }
 
+    /**
+     * getter method for strategy
+     * @return the strategy used by the player, null if the player is a human player
+     * @author u7620014 Haobo Zou
+     */
     public Strategy getStrategy() {
         return this.strategy;
     }
 
+    /**
+     * setter method for strategy
+     * @param strategy the strategy used by the player, can be null if the player is a human player
+     * @author u7620014 Haobo Zou
+     */
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
     }
@@ -116,10 +130,19 @@ public class Player {
         return "P" + this.colour.getColourChar() + String.format("%03d", this.dirham) + String.format("%02d", this.numOfUnplacedRugs);
     }
 
+    /**
+     * Strategy enum represents different strategies used by the computer player
+     * @author u7620014 Haobo Zou
+     */
     public enum Strategy {
         RANDOM,
         INTELLIGENT;
 
+        /**
+         * Converts a Strategy enum to a string of its full name
+         * @return full name of the strategy
+         * @author u7620014 Haobo Zou
+         */
         @Override
         public String toString() {
             return switch (this) {
