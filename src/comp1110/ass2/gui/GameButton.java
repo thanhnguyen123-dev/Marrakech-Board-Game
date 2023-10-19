b40d40e0ca1033f9caed9422dccc71117d82e4de
Haobo Zou <zouhaobo@hotmail.com>
2023-10-19 17:58:41 +1100

    Added more Javadoc for GameButton class.

--- src/comp1110/ass2/gui/GameButton.java
+++ src/comp1110/ass2/gui/GameButton.java
@@ -6,10 +6,17 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
 /**
- * Customized rectangle buttons
+ * a custom button class used for player interaction inside Game class
  * @author u7620014 Haobo Zou, u7582846 Yaolin Li
  */
 class GameButton extends Button {
+    /**
+     * Constructor: creates an instance of the GameButton class
+     * @param string the text to be displayed inside the button
+     * @param width a fixed width
+     * @param height a fixed height
+     * @author u7620014 Haobo Zou, u7582846 Yaolin Li
+     */
     public GameButton(String string, double width, double height) {
         super(string);
         this.setStyle("-fx-text-fill: white;-fx-background-color: #672F09;");
